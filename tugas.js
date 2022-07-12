fe88af97b8bf8f3f30c2958f746dc933b6d9a12c
faizbyp <faizbyp@gmail.com>
2022-07-12 13:37:39 +0700

    mengubah style dan formatting

--- tugas.js
+++ tugas.js
@@ -7,7 +7,6 @@ form.addEventListener('submit', tambahTugas);
 // Elemen yang di-select adalah seluruh daftar tugas
 daftarTugas.addEventListener('click', hapusTugas);
 
-
 function tambahTugas(e){
     // Mencegah form melakukan submit (get/post)
     e.preventDefault();
@@ -19,25 +18,33 @@ function tambahTugas(e){
         alert('Nama tugas tidak boleh kosong');
     }
     else{
-        // Membuat <li>
-        var li = document.createElement('li');
-        // Menambah atribut class pada <li>
-        li.setAttribute('class', 'tugas');
-        // Memasukkan value nama tugas ke <li>
-        li.appendChild(document.createTextNode(namaTugas));
+        // Membuat table row
+        var tr = document.createElement('tr');
+
+        // Membuat table data tugas
+        var tdTugas = document.createElement('td');
+        // Menambah atribut class pada table data tugas
+        tdTugas.setAttribute('class', 'tugas');
+        // Memasukkan value nama tugas ke table data tugas
+        tdTugas.appendChild(document.createTextNode(namaTugas));
         
+        // Membuat table data hapus
+        var tdHapus = document.createElement('td');
         // Membuat tombol hapus
         var hapus = document.createElement('button');
         // Menambah atribut class pada tombol hapus
         hapus.setAttribute('class', 'hapus-btn');
         // Menambah icon ceklis pada tombol hapus
         hapus.appendChild(document.createTextNode('✔'));
-        
-        // Memasukkan tombol hapus ke <li>
-        li.appendChild(hapus);
+        // Memasukkan tombol hapus ke table data hapus
+        tdHapus.appendChild(hapus);
+
+        // Memasukkan 2 table data ke dalam table row
+        tr.appendChild(tdTugas);
+        tr.appendChild(tdHapus);
     
-        // Memasukkan <li> baru ke <ul> daftar tugas
-        daftarTugas.appendChild(li);
+        // Memasukkan table row baru ke table daftar tugas
+        daftarTugas.appendChild(tr);
     
         console.log('Berhasil menambah tugas: ' + namaTugas);
     }
@@ -45,16 +52,16 @@ function tambahTugas(e){
 }
 
 function hapusTugas(e){
-    // Jika target yang diklik adalah elemen dengan class hapus-btn, maka ...
+    // Jika target yang diklik adalah elemen dengan class hapus-btn, maka jalankan kode
     if(e.target.classList.contains('hapus-btn')){
         // Konfirmasi sebelum menghapus
         if(confirm('Yakin tugas sudah selesai?')){
-            // Membuat variabel element parent dari class hapus-btn
-            var li = e.target.parentElement;
+            // Membuat variabel element grandparent (parent dari parent) dari class hapus-btn
+            var tr = e.target.parentElement.parentElement;
             // Menghapus parent dari class hapus-btn, yaitu <li>
-            daftarTugas.removeChild(li);
+            daftarTugas.removeChild(tr);
             
-            console.log('Berhasil menghapus tugas: ' + li.textContent);
+            console.log('Berhasil menghapus tugas: ' + tr.children[0].textContent);
         }
     }
 }
